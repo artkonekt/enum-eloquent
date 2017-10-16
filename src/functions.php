2385cb83d2c87b768f1a61c3e1b0fab99d9e92e9
Attila Fulop <fulopattila122@gmail.com>
2017-10-16 12:58:53 +0300

    Added support for `str_after` on Laravel < v5.4

--- src/functions.php
+++ src/functions.php
@@ -1,6 +1,8 @@
 <?php
 /**
- * Add support for str_before on Laravel < v5.5
+ * Add support for:
+ * - str_before on Laravel < v5.5
+ * - str_after  on Laravel < v5.4
  */
 
 if (!function_exists('str_before')) {
@@ -14,5 +16,19 @@ if (!function_exists('str_before')) {
     function str_before($subject, $search)
     {
         return $search === '' ? $subject : explode($search, $subject)[0];
+    }
+}
+
+if (!function_exists('str_after')) {
+    /**
+     * Return the remainder of a string after a given value.
+     *
+     * @param  string  $subject
+     * @param  string  $search
+     * @return string
+     */
+    function str_after($subject, $search)
+    {
+        return $search === '' ? $subject : array_reverse(explode($search, $subject, 2))[0];
     }
 }
