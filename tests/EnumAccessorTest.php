cd9bc3818212aa266d7524d48ca1f1d098653c5b
Mark Boessenkool <mark@boldbeard.nl>
2019-09-03 18:03:51 +0200

    fixed build issues with coding standards

--- tests/EnumAccessorTest.php
+++ tests/EnumAccessorTest.php
@@ -43,8 +43,7 @@ class EnumAccessorTest extends TestCase
     public function it_returns_the_enum_default_when_attribute_is_null()
     {
         // don't test if mayor version is lower than 3
-        if ($this->getEnumVersionMayor() < 3)
-        {
+        if ($this->getEnumVersionMayor() < 3) {
             $this->assertTrue(true);
             return;
         }
@@ -63,8 +62,7 @@ class EnumAccessorTest extends TestCase
     public function it_returns_the_enum_v2_default_when_attribute_is_null()
     {
         // don't test if mayor version is 3 or higher
-        if ($this->getEnumVersionMayor() >= 3)
-        {
+        if ($this->getEnumVersionMayor() >= 3) {
             $this->assertTrue(true);
             return;
         }
